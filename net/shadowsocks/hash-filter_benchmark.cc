c8e859f8ee3dfefb854b49f5fd5030810f268b1a
iceboy <me@iceboy.org>
2021-02-27 20:33:45 -0800

    net/shadowsocks:hash-filter_benchmark refine

--- net/shadowsocks/hash-filter_benchmark.cc
+++ net/shadowsocks/hash-filter_benchmark.cc
@@ -10,26 +10,34 @@ namespace {
 
 void BM_insert(benchmark::State &state) {
     absl::BitGen gen;
+    HashFilter filter;
+    int64_t count = 0;
     for (auto _ : state) {
-        HashFilter filter;
-        for (int64_t i = 0; i < state.range(0); ++i) {
-            filter.insert(absl::Uniform<uint64_t>(gen));
+        if (++count >= state.range(0)) {
+            filter.clear();
+            count = 0;
         }
+        filter.insert(absl::Uniform<uint64_t>(gen));
     }
-    state.SetItemsProcessed(state.iterations() * state.range(0));
 }
 
 void BM_test(benchmark::State &state) {
     absl::BitGen gen;
     HashFilter filter;
+    for (int64_t i = 0; i < state.range(0); ++i) {
+        filter.insert(absl::Uniform<uint64_t>(gen));
+    }
     for (auto _ : state) {
         benchmark::DoNotOptimize(filter.test(absl::Uniform<uint64_t>(gen)));
     }
 }
 
 BENCHMARK(BM_insert)
-    ->Arg(600000)->Arg(700000)->Arg(800000)->Arg(900000)->Arg(950000);
-BENCHMARK(BM_test);
+    ->Arg(100000)->Arg(200000)->Arg(400000)->Arg(600000)->Arg(800000)
+    ->Arg(900000)->Arg(950000)->Arg(1000000);
+BENCHMARK(BM_test)
+    ->Arg(0)->Arg(100000)->Arg(200000)->Arg(400000)->Arg(600000)->Arg(800000)
+    ->Arg(900000)->Arg(950000)->Arg(1000000);
 
 }  // namespace
 }  // namespace shadowsocks

d4c574b8761f8c006801442b453498c99a64793e
iceboy <me@iceboy.org>
2026-07-01 19:54:34 -0700

    read network and host files

--- src/configs/route.rs
+++ src/configs/route.rs
@@ -1,4 +1,10 @@
-use std::{io, sync::Arc};
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::{Path, PathBuf},
+    str::FromStr,
+    sync::Arc,
+};
 
 use ip_network::IpNetwork;
 use serde::Deserialize;
@@ -17,10 +23,19 @@ pub struct RouteConnectorRuleConfig {
     networks: Vec<IpNetwork>,
 
     #[serde(default)]
+    network_files: Vec<PathBuf>,
+
+    #[serde(default)]
     hosts: Vec<String>,
 
     #[serde(default)]
+    host_files: Vec<PathBuf>,
+
+    #[serde(default)]
     host_suffixes: Vec<String>,
+
+    #[serde(default)]
+    host_suffix_files: Vec<PathBuf>,
 
     #[serde(default)]
     default: bool,
@@ -40,6 +55,18 @@ pub fn init(registry: &mut Registry) {
             .map_err(|_| io::Error::new(io::ErrorKind::InvalidData, "invalid connector config"))?;
         let mut builder = RouteConnectorBuilder::new();
         for rule in &c.rules {
+            let mut networks = rule.networks.clone();
+            for file in &rule.network_files {
+                read_network_file(file, &mut networks)?;
+            }
+            let mut hosts = rule.hosts.clone();
+            for file in &rule.host_files {
+                read_host_file(file, &mut hosts)?;
+            }
+            let mut host_suffixes = rule.host_suffixes.clone();
+            for file in &rule.host_suffix_files {
+                read_host_file(file, &mut host_suffixes)?;
+            }
             let connector = if rule.drop {
                 None
             } else {
@@ -48,8 +75,34 @@ pub fn init(registry: &mut Registry) {
             if rule.default {
                 builder.set_default_connector(connector.clone());
             }
-            builder.add_rule(&rule.networks, &rule.hosts, &rule.host_suffixes, connector);
+            builder.add_rule(&networks, &hosts, &rule.host_suffixes, connector);
         }
         Ok(Arc::new(builder.build()))
     });
+}
+
+fn read_network_file(file: &Path, networks: &mut Vec<IpNetwork>) -> io::Result<()> {
+    for line_result in io::BufReader::new(File::open(file)?).lines() {
+        let line = line_result?;
+        let line = line.trim();
+        if line.is_empty() || line.starts_with('#') {
+            continue;
+        }
+        networks.push(
+            IpNetwork::from_str(line).map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))?,
+        );
+    }
+    Ok(())
+}
+
+fn read_host_file(file: &Path, hosts: &mut Vec<String>) -> io::Result<()> {
+    for line_result in io::BufReader::new(File::open(file)?).lines() {
+        let line = line_result?;
+        let line = line.trim();
+        if line.is_empty() || line.starts_with('#') {
+            continue;
+        }
+        hosts.push(line.to_string());
+    }
+    Ok(())
 }
